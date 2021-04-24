9636558ceded68fac9e381cff8183c2798ccc0e1
donald kaiser <donaldkaiserjr@gmail.com>
2021-04-24 14:09:38 -0700

    updated script.js file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /* */
+const codes = document.querySelectorAll('.code')
+
+codes[0].focus()  //note: this puts the cursor in the first number box when you got to the actual page
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <=9) {
+            codes[idx].value = ''
+            setTimeout(() => codes[idx + 1].focus(), 10)
+        } else if(e.key === 'Backspace') {
+            setTimeout(() => codes[idx - 1].focus(), 10)
+        }
+    })
+})
